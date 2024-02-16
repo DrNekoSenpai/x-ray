2f1d772e0ac5618136ce46e37883f2daad240898
DrNekoSenpai <haesungbecker@gmail.com>
2024-02-15 20:36:00 -0800

    Major changes to main/alt hierarchy system

--- analysis.py
+++ analysis.py
@@ -1,45 +1,198 @@
-import os
+import os, re, datetime, pickle
 logs = [file[:-4] for file in os.listdir("./logs/") if not "_input" in file]
 
 immune = [ 
     "Sned",
-    "Kami", 
     "BumblinMumbler",
-    "Bran6",
-    "Chalk Outl", 
-    "Hardcastle",
-    "A Teen",
-    "Your Angry",
-    "Perfect",
-    "Stunted Na",
-    "Demeter's",
-    "Loo Czar",
-    "SickSix6",
-    "Procrastin",
-    "Vixi's Cur",
-    "TENENTEN",
-    "DisasterBa",
-    "Thumb Salu",
-    "Opposable",
-    "Sulfur",
-    "Tenth Situ",
-    "Seven Thun",
-    "Not My Nam",
-    "Clone Cast",
-    "Dark Hell",
-    "Hostile Do",
-    "Fearless TH9"
+    "Glowy Gore"
 ]
+# To be deprecated, claims system in progress.
+
+with open("claims-xray.txt", "r", encoding="utf-8") as file: 
+    xray_claims = file.readlines()
+
+with open("claims-outlaws.txt", "r", encoding="utf-8") as file:
+    outlaws_claims = file.readlines()
+
+with open("minion-xray.txt", "r", encoding="utf-8") as file:
+    xray_data = file.readlines()
+
+with open("minion-outlaws.txt", "r", encoding="utf-8") as file:
+    outlaws_data = file.readlines()
 
 def regular_keyboard(input_string): 
-    pattern = r"^[A-Za-z0-9 !@#$%^&*()\-=\[\]{}|;:'\",.<>/?\\_+]*$"
+    pattern = r"^[A-Za-z0-9 \~!@#$%^&*()\-=\[\]{}|;:'\",.<>/?\\_+]*$"
     return re.match(pattern, input_string) is not None 
+
+# 15 #P2UPPVYL    ‭⁦Sned      ⁩‬ Sned | PST
+claims_pattern = re.compile(r"(\d{1,2})\s+#([A-Z0-9]{5,9})\s+‭⁦(.*)⁩‬(.*)")
+
+# #P2UPPVYL    15 Sned
+full_account_name_pattern = re.compile(r"([A-Z0-9]{5,9})\s+\d{1,2}\s+(.*)")
+
+class Claim: 
+    def __init__(self, town_hall:int, tag:str, name:str, is_main:bool, clan:str): 
+        self.tag = tag
+        self.town_hall = town_hall
+        self.name = name
+        self.is_main = is_main
+        self.clan = clan
+
+claims_dictionary = {}
+
+for claim in xray_claims: 
+    claim_th, claim_tag, claim_name, claimer = re.search(claims_pattern, claim).groups()
+
+    claim_tag = claim_tag.strip()
+    claim_name = claim_name.strip()
+    claimer = claimer.strip()
+
+    for account in xray_data: 
+
+        account_tag, account_name = re.search(full_account_name_pattern, account).groups()
+
+        account_tag = account_tag.strip()
+        account_name = account_name.strip()
+
+        if account_name == "JALVIN ø": account_name = "JALVIN"
+        if account_name == "★ıċєʏקѧṅṭś★": account_name = "IceyPants"
+        if account_name == "General⚡️Mc0⚡️": account_name = "General Mc0"
+        if account_name == "༺༃༼SEV༽༃༻": account_name = "SEV"
+        if account_name == "「 NightEye 」": account_name = "NightEye"
+        if account_name == "Mini @ñ@$": account_name = "Mini Anas"
+        if account_name == "❤️lav❤️": account_name = "lav"
+        if account_name == "$õckÕ": account_name = "Socko"
+        
+        if "’" in account_name: account_name = account_name.replace("’", "'")
+        if "™" in account_name: account_name = account_name.replace("™", "")
+        if "✨" in account_name: account_name = account_name.replace("✨", "")
+        if "\_" in account_name: account_name = account_name.replace("\_", "_")
+        if "\~" in account_name: account_name = account_name.replace("\~", "~")
+
+        if not regular_keyboard(account_name):
+            print(f"Player name '{account_name}' is not valid. Please input the name manually.")
+            account_name = input("Name: ")
+
+        if account_tag == claim_tag: 
+            if claimer not in claims_dictionary: claims_dictionary[claimer] = []
+            claims_dictionary[claimer].append(Claim(claim_th, claim_tag, account_name, False, "Reddit X-Ray"))
+            break
+
+for claim in outlaws_claims:
+    claim_th, claim_tag, claim_name, claimer = re.search(claims_pattern, claim).groups()
+
+    claim_tag = claim_tag.strip()
+    claim_name = claim_name.strip()
+    claimer = claimer.strip()
+
+    for account in outlaws_data: 
+        account_tag, account_name = re.search(full_account_name_pattern, account).groups()
+
+        account_tag = account_tag.strip()
+        account_name = account_name.strip()
+
+        if account_name == "JALVIN ø": account_name = "JALVIN"
+        if account_name == "★ıċєʏקѧṅṭś★": account_name = "IceyPants"
+        if account_name == "General⚡️Mc0⚡️": account_name = "General Mc0"
+        if account_name == "༺༃༼SEV༽༃༻": account_name = "SEV"
+        if account_name == "「 NightEye 」": account_name = "NightEye"
+        if account_name == "Mini @ñ@$": account_name = "Mini Anas"
+        if account_name == "❤️lav❤️": account_name = "lav"
+        if account_name == "$õckÕ": account_name = "Socko"
+        
+        if "’" in account_name: account_name = account_name.replace("’", "'")
+        if "™" in account_name: account_name = account_name.replace("™", "")
+        if "✨" in account_name: account_name = account_name.replace("✨", "")
+        if "\_" in account_name: account_name = account_name.replace("\_", "_")
+        if "\~" in account_name: account_name = account_name.replace("\~", "~")
+
+        if not regular_keyboard(account_name):
+            print(f"Player name '{account_name}' is not valid. Please input the name manually.")
+            account_name = input("Name: ")
+
+        if account_tag == claim_tag: 
+            if claimer not in claims_dictionary: claims_dictionary[claimer] = []
+            claims_dictionary[claimer].append(Claim(claim_th, claim_tag, account_name, False, "Faint Outlaws"))
+            break
+
+known_mains = ["Glowy Gore", "Lil Ank"]
+
+with open("claims_output.txt", "w", encoding="utf-8") as file:
+    for claimer in claims_dictionary: 
+        # main_exists is a tuple of two booleans. 
+        # The first is whether or not the claimer has a main in Reddit X-ray. 
+        # The second is whether or not the claimer has a main in Faint Outlaws.
+        # None -- no accounts in this clan
+        # True -- a main account has been identified
+        # False -- no main account has been identified
+
+        for claim in claims_dictionary[claimer]:
+            accounts_xray = [claim for claim in claims_dictionary[claimer] if claim.clan == "Reddit X-Ray"]
+            accounts_outlaws = [claim for claim in claims_dictionary[claimer] if claim.clan == "Faint Outlaws"]
+            accounts_total = accounts_xray + accounts_outlaws
+
+            num_accounts_xray = len(accounts_xray)
+            num_accounts_outlaws = len(accounts_outlaws)
+            num_accounts_total = len(accounts_total)
+
+            if num_accounts_total == 1: 
+                main_account = accounts_total[0]
+                main_account.is_main = True
+
+                claims_dictionary[claimer] = [main_account] + [claim for claim in claims_dictionary[claimer] if claim.tag != main_account.tag]
+
+            # Otherwise, if they have one account in Reddit X-ray, set that one as main. 
+            elif num_accounts_xray == 1:
+                main_account = accounts_xray[0]
+                main_account.is_main = True
+
+                claims_dictionary[claimer] = [main_account] + [claim for claim in claims_dictionary[claimer] if claim.tag != main_account.tag]
+
+            # If they have multiple accounts in Reddit X-ray, set the one with the highest town hall level as main.
+            elif num_accounts_xray > 1:
+                main_account = max(accounts_xray, key=lambda account: account.town_hall)
+                main_account.is_main = True
+
+                claims_dictionary[claimer] = [main_account] + [claim for claim in claims_dictionary[claimer] if claim.tag != main_account.tag]
+
+            else: 
+                for account in accounts_total:
+                    if account.name in known_mains: 
+                        account.is_main = True
+                        break
+
+                if len([account for account in accounts_total if account.is_main]) == 0:
+                    # At this point, we have still not identified a main account. 
+                    # Set the account with the highest town hall level as the main account.
+                    main_account = max(accounts_total, key=lambda account: account.town_hall)
+                    main_account.is_main = True
+
+                    claims_dictionary[claimer] = [main_account] + [account for account in accounts_total if account.tag != main_account.tag]
+
+                else: 
+                    claims_dictionary[claimer] = [account for account in accounts_total if account.is_main] + [account for account in accounts_total if not account.is_main]
+
+    for claimer in claims_dictionary: 
+        accounts_xray = [claim for claim in claims_dictionary[claimer] if claim.clan == "Reddit X-Ray"]
+        accounts_outlaws = [claim for claim in claims_dictionary[claimer] if claim.clan == "Faint Outlaws"]
+        accounts_total = accounts_xray + accounts_outlaws
+
+        num_accounts_xray = len(accounts_xray)
+        num_accounts_outlaws = len(accounts_outlaws)
+        num_accounts_total = len(accounts_total)
+
+        file.write(f"{claimer}: {num_accounts_xray} accounts in Reddit X-Ray, {num_accounts_outlaws} accounts in Faint Outlaws\n")
+        for account in accounts_total: 
+            file.write(f"  - {account.name} ({account.town_hall}) -- {account.clan}")
+            if account.is_main: file.write(" (main)")
+            file.write("\n")
+
+        file.write("\n")
 
 for log_file in logs: 
     with open(f"./logs/{log_file}.txt", "r", encoding="utf-8") as file: 
         lines = file.readlines()
 
-    import re, datetime
     hit_pattern = re.compile(r":(\d{2})::\d{2}::Sword::(\d{2})::\d{2}:(:Star:|:FadedStar:|:Blank:)(:Star:|:FadedStar:|:Blank:)(:Star:|:FadedStar:|:Blank:):\d{2,3}:[💥]? .{2}(.*).{2}")
 
     win_loss_pattern = re.compile(r"Win/loss: (win|loss|blacklist win|blacklist loss)")
@@ -128,7 +281,6 @@ for log_file in logs:
 
         if attack_list == 2: 
             player_name = line.strip()[8:]
-            print(f"{player_name} missed two hits")
 
             if player_name == "JALVIN ø": player_name = "JALVIN"
             if player_name == "★ıċєʏקѧṅṭś★": player_name = "IceyPants"
@@ -143,7 +295,6 @@ for log_file in logs:
 
         if attack_list == 1:
             player_name = line.strip()[8:]
-            print(f"{player_name} missed one hit")
 
             if player_name == "JALVIN ø": player_name = "JALVIN"
             if player_name == "★ıċєʏקѧṅṭś★": player_name = "IceyPants"
@@ -162,7 +313,32 @@ for log_file in logs:
             for entry in log: 
                 player_name, attacker, defender, stars, time_remaining = entry
                 # print(f"Player: {player_name}, Attacker: {attacker}, Defender: {defender}, Stars: {stars}, Time Remaining: {time_remaining:.2f}")
-                if player_name in immune or "Unicorn" in player_name: continue
+                if player_name in immune or "Unicorn" in player_name: 
+                    continue
+
+                is_main = True
+                for claimer in claims_dictionary: 
+                    for account in claims_dictionary[claimer]: 
+                        if account.name == player_name: 
+                            if not account.is_main: 
+                                is_main = False
+                                
+                if not is_main: continue
+
+                # Check if an account with the same name exists in the claims dictionary.
+                # If not, they probably left the clan. 
+                account_found = False
+                for claimer in claims_dictionary:
+                    for account in claims_dictionary[claimer]: 
+                        if account.name == player_name: 
+                            account_found = True
+                            break
+                    if account_found: break
+
+                if not account_found:
+                    print(f"Bypass: {player_name} appears to have left")
+                    continue
+
                 if int(defender) > 5: 
                     # Check if this hit was not a mirror 
                     mirror = attacker == defender
@@ -175,11 +351,16 @@ for log_file in logs:
                         elif win_loss == "win" and stars < 3:
                             print(f"Bypass: #{attacker} {player_name} hit someone not their own mirror, but this appears to be a snipe")
                         
+                        # Next, check if they already hit their mirror, but the defender's number is not in the invalid_mirror list.
+                        # This can occur if they hit their mirror second; and thus we should not penalize.
+                        elif attacker in [entry[2] for entry in log if entry[1] == attacker]: 
+                            print(f"Bypass: #{attacker} {player_name} hit someone not their own mirror, but they already hit their mirror")
+                            
                         # If not, first check if the defender's number is in the invalid_mirror list. 
                         # This means that their own mirror was already taken, and thus we should not penalize. 
                         elif attacker in invalid_mirror:
                             print(f"Bypass: #{attacker} {player_name} hit someone not their own mirror, but their mirror was already taken")
-                            
+
                         # If the defender's number is not in the invalid_mirror list, then we should add it.
                         # This means that their own mirror was not taken, and thus we should penalize.
                         else:
@@ -226,7 +407,21 @@ for log_file in logs:
                     continue
 
                 # Find the corresponding entry in the log; find their other hit 
-                if entry in immune or "Unicorn" in entry: continue
+                if entry in immune or "Unicorn" in entry: 
+                    print(f"Bypass: {entry} missed one hit, but they are immune")
+                    continue
+
+                is_main = True
+                for claimer in claims_dictionary:
+                    for account in claims_dictionary[claimer]: 
+                        if account.name == entry: 
+                            if not account.is_main: 
+                                is_main = False
+
+                if not is_main: 
+                    print(f"Bypass: {entry} missed one hit, but they are not a main account")
+                    continue
+
                 for log_entry in log: 
                     if log_entry[0] == entry: 
                         mirror = log_entry[1] == log_entry[2]
@@ -235,21 +430,89 @@ for log_file in logs:
                             file.write(f"3\n{entry}\ny\n1\n{enemy_clan}\n{war_end_date}\n")
 
             for entry in two_missed_hits: 
-                if entry in immune or "Unicorn" in entry: continue
+                if entry in immune or "Unicorn" in entry: 
+                    print(f"Bypass: {entry} missed two hits, but they are immune")
+                    continue
+
+                is_main = True
+                for claimer in claims_dictionary:
+                    for account in claims_dictionary[claimer]: 
+                        if account.name == entry: 
+                            if not account.is_main: 
+                                is_main = False
+                if not is_main: 
+                    print(f"Bypass: {entry} missed two hits, but they are not a main account")
+                    continue
+
                 print(f"Warning: {entry} missed two hits")
                 file.write(f"3\n{entry}\ny\n1\n{enemy_clan}\n{war_end_date}\n")
 
         elif win_loss == "blacklist win" or win_loss == "blacklist loss":
             victory = "y" if win_loss.split(" ")[1] == "win" else "n"
             for entry in one_missed_hit: 
-                if entry in immune or "Unicorn" in entry: continue
+                if entry in immune or "Unicorn" in entry: 
+                    print(f"Bypass: {entry} missed one hit on a blacklist war, but they are immune")
+                    continue
+
+                # Check if an account with the same name exists in the claims dictionary.
+                # If not, they probably left the clan. 
+                account_found = False
+                for claimer in claims_dictionary:
+                    for account in claims_dictionary[claimer]: 
+                        if account.name == entry: 
+                            account_found = True
+                            break
+                    if account_found: break
+
+                if not account_found:
+                    print(f"Bypass: {entry} appears to have left")
+                    continue
+
+                is_main = True
+                for claimer in claims_dictionary:
+                    for account in claims_dictionary[claimer]: 
+                        if account.name == entry: 
+                            if not account.is_main: 
+                                is_main = False
+                if not is_main: 
+                    print(f"Bypass: {entry} missed one hit on a blacklist war, but they are not a main account")
+                    continue
+
                 if victory == "y": 
                     print(f"Bypass: {entry} missed one hit on a blacklist war, but we won anyway")
                 else: 
                     print(f"Warning: {entry} missed one hit on a blacklist war")
                     file.write(f"3\n{entry}\ny\n3\n{enemy_clan}\nn\n1\n")
+
             for entry in two_missed_hits: 
-                if entry in immune or "Unicorn" in entry: continue
+                if entry in immune or "Unicorn" in entry: 
+                    print(f"Bypass: {entry} missed two hits on a blacklist war, but they are immune")
+                    continue
+
+                # Check if an account with the same name exists in the claims dictionary.
+                # If not, they probably left the clan. 
+                account_found = False
+                for claimer in claims_dictionary:
+                    for account in claims_dictionary[claimer]: 
+                        if account.name == entry: 
+                            account_found = True
+                            break
+                    if account_found: break
+
+                if not account_found:
+                    print(f"Bypass: {entry} appears to have left")
+                    continue
+
+                is_main = True
+                for claimer in claims_dictionary:
+                    for account in claims_dictionary[claimer]: 
+                        if account.name == entry: 
+                            if not account.is_main: 
+                                is_main = False
+                if not is_main: 
+                    print(f"Bypass: {entry} missed two hits on a blacklist war, but they are not a main account")
+                    continue
+
                 if victory == "y": 
                     print(f"Warning: {entry} missed two hits on a blacklist war, but we still won")
                     file.write(f"3\n{entry}\ny\n3\n{enemy_clan}\ny\n2\n")
