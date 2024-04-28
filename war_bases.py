90740e54cc8ebe893f5a63d9cb4ecd9fcae21905
DrNekoSenpai <haesungbecker@gmail.com>
2024-04-28 01:20:36 -0700

    Strikes 04/28; revised for blacklist conditional

--- war_bases.py
+++ war_bases.py
@@ -5,14 +5,14 @@ left, right, up, down = 148, 389, 50, 94
 
 name = pyautogui.screenshot().crop((left, up, right, down))
 
-def preprocess_image(image, threshold=225):
+def black_and_white(image, threshold=225):
     image = np.array(image)
     mask = (image > threshold).all(axis=-1)
     image[mask] = [0, 0, 0]
     image[~mask] = [255, 255, 255]
     return Image.fromarray(image, 'RGB')
 
-name = preprocess_image(name)
+name = black_and_white(name)
 name.show()
 
 name = pytesseract.image_to_string(name)
